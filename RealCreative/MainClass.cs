f7803a30ce8a6fe015eeb3b94dde41d34f46ad0a
ITR <ITR@live.no>
2021-06-29 19:44:58 +0200

    Added menu button

--- RealCreative/MainClass.cs
+++ RealCreative/MainClass.cs
@@ -1,8 +1,21 @@
 ﻿using MelonLoader;
+using UnityEngine;
 
 namespace RealCreative
 {
     public class MainClass : MelonMod
     {
+        private GameObject _tempTest;
+        public override void OnUpdate()
+        {
+            if (_tempTest != null) return;
+            _tempTest = UiInjector.CreateSettingsButton(
+                "Test",
+                () => { MelonLogger.Msg("Button was clicked"); }
+            );
+
+            if (_tempTest == null) return;
+            MelonLogger.Msg("Created button");
+        }
     }
 }
---
+++ RealCreative/UiInjector.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using Object = UnityEngine.Object;
+
+namespace RealCreative
+{
+    public static class UiInjector
+    {
+        public static GameObject CreateSettingsButton(string text, UnityAction onClick)
+        {
+            var otherInput = OtherInput.Instance;
+            if (otherInput == null) return null;
+
+            var clone = CloneResume(otherInput);
+            if (clone == null) return null;
+
+            if (SetupButton(clone, text, onClick)) return clone;
+
+            Object.Destroy(clone);
+            return null;
+        }
+
+        private static GameObject CloneResume(OtherInput otherInput)
+        {
+            if (otherInput.pauseUi == null)
+            {
+                MelonLogger.Error("pauseUi not assigned");
+                return null;
+            }
+
+            var pauseTransform = otherInput.pauseUi.transform;
+            var originalChild = pauseTransform.GetChild(0);
+            if (originalChild == null)
+            {
+                MelonLogger.Error("Failed to find children on pauseUi");
+                return null;
+            }
+
+            return Object.Instantiate(originalChild.gameObject, pauseTransform.transform);
+        }
+
+        private static bool SetupButton(GameObject root, string text, UnityAction onClick)
+        {
+            root.name = text + "Btn";
+            var textComponent = root.GetComponentInChildren<TextMeshProUGUI>();
+            if (textComponent == null)
+            {
+                MelonLogger.Error("Failed to find text component for resume button");
+                return false;
+            }
+
+            textComponent.text = text;
+
+            // Doesn't need inChildren, just there for future-proofing
+            var buttonComponent = root.GetComponentInChildren<Button>();
+            if (buttonComponent == null)
+            {
+                MelonLogger.Error("Failed to find button component for resume button");
+                return false;
+            }
+
+            buttonComponent.onClick.RemoveAllListeners();
+            buttonComponent.onClick.AddListener(onClick);
+            return true;
+        }
+    }
+}
